0814d20dfdb248feccb9ecb0c998f7a3d4c5259b
Young Jun Park <june0313@gmail.com>
2021-06-06 07:44:00 +0900

    version 1

---
+++ src/main/java/com/example/object/application/Invitation.java
@@ -0,0 +1,7 @@
+package com.example.object.application;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/main/java/com/example/object/application/Ticket.java
@@ -0,0 +1,9 @@
+package com.example.object.application;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
