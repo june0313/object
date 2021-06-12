90a37f338589c190b54f5c4809fb3cc6986853f6
Young Jun Park <june0313@gmail.com>
2021-06-12 14:29:01 +0900

    책임 주도 설계 - Movie 개선

--- object-chapter5/src/main/java/com/example/object/chapter5/Movie.java
+++ object-chapter5/src/main/java/com/example/object/chapter5/Movie.java
@@ -1,53 +1,36 @@
 package com.example.object.chapter5;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
-    private String title;
-    private Duration runningTime;
-    private Money fee;
-    private List<DiscountCondition> discountConditions;
+public abstract class Movie {
+    private final String title;
+    private final Duration runningTime;
+    private final Money fee;
+    private final List<DiscountCondition> discountConditions;
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
-
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable(screening)) {
             return fee.minus(calculateDiscountAmount());
         }
-        return null;
-    }
-
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-        }
-
-        throw new IllegalStateException();
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
+        return fee;
     }
 
     private boolean isDiscountable(Screening screening) {
-        return discountConditions.stream()
-                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+        return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    abstract protected Money calculateDiscountAmount();
+
+    protected Money getFee() {
+        return this.fee;
     }
 }
---
+++ object-chapter5/src/main/java/com/example/object/chapter5/NoneDiscountMovie.java
@@ -0,0 +1,14 @@
+package com.example.object.chapter5;
+
+import java.time.Duration;
+
+public class NoneDiscountMovie extends Movie {
+    public NoneDiscountMovie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        super(title, runningTime, fee, discountConditions);
+    }
+
+    @Override
+    protected Money calculateDiscountAmount() {
+        return Money.ZERO;
+    }
+}
